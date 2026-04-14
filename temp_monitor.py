36d1195ae59cdbe55ecfe616bb79f8f61c3f8f2b
juanrmv528 <juanramboy1@gmail.com>
2026-04-14 13:19:01 -0500

    1

--- temp_monitor.py
+++ temp_monitor.py
@@ -20,7 +20,7 @@ def add_reading(monitor, temp):
     Agrega una nueva lectura con la temperatura especificada.
     Retorna el diccionario modificado.
     """
-    # TODO: Implementar
+    # TODO: Implementa
     pass
 
 
