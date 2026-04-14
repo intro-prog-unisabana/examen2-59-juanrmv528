c5c21cebd6e0bcea91c6f4f0da7a4a710b9805cb
juanrmv528 <juanramboy1@gmail.com>
2026-04-14 14:37:36 -0500

    10

--- temp_monitor_client.py
+++ temp_monitor_client.py
@@ -7,30 +7,9 @@ import temp_monitor
 
 def main():
     # TODO: Pedir el nombre del archivo al usuario usando input()
-
-temp_monitor.init()
-
-bogota_dia.txt[12, 8.0, 9.5, 11.0, 13.5, 15, 17.5, 19.0, 20.0, 19.5, 18.0,16.5 15.0]
-cartagena_dia.txt[12, 26.0, 27.5, 29.0, 30.5, 31]
-
-def name()
-    
-    input(bogota_dia.txt)
-
-def name()
-    
-    input(cartagena_dia.txt)
-
-def name()
-    
-    input(moscow_semana.txt)
-
-
     
     # TODO: Abrir el archivo y leer el numero de lecturas n
 
-
-    
     # TODO: Crear el monitor usando temp_monitor.init(n)
     
     # TODO: Leer las n temperaturas y agregarlas con temp_monitor.add_reading()
